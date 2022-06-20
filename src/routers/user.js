a3423f8732e8a9f0ebf93d9684e3f865bf31707f
BrandonMrgichCognizant <Brandon.Mrgich@cognizant.com>
2022-06-20 12:09:31 -0400

    Init commit

---
+++ src/routers/user.js
@@ -0,0 +1,139 @@
+const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+const { sendWelcomeEmail, sendGoodbyeEmail } = require('../emails/account')
+const router = new express.Router()
+
+// Create account
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    
+    try {
+        await user.save()
+        sendWelcomeEmail(user.email, user.name)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token} )
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// Login
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+// Logout cur token
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// Logout all tokens
+router.post('/users/logoutAll', auth, async(req,res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// Gets own profile
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
+})
+
+// Update user
+router.patch('/users/me', auth, async (req,res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email','password','age']
+    const isValidOp = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOp) return res.status(400).send({ error: 'Invalid updates.' })
+
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// Delete user
+router.delete('/users/me', auth, async (req, res) => {
+    try {
+        await req.user.remove()
+        sendGoodbyeEmail(req.user.email, req.user.name)
+        res.send(req.user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+// Photo restrictions
+const upload = multer({
+    limits: {
+        fileSized: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('must be img'))
+        }
+        cb (undefined, true)
+    }
+})
+
+// Create/update profile pics
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (err, req, res, next) => {
+    res.status(400).send({ err: err.message })
+})
+
+// Delete profile pic
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    console.log('before:' + req.user.avatar)
+    req.user.avatar = undefined
+    console.log('after: ' + req.user.avatar)
+    await req.user.save()
+    res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
+})
+
+module.exports = router
