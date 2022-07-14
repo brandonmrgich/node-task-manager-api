51bd4bee455c0c0e5181c94ad1209c47b45ff842
BrandonMrgichCognizant <Brandon.Mrgich@cognizant.com>
2022-07-14 13:15:14 -0400

    automated email fix

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email, 
-        from: 'mrgich@mail.io',
+        from: 'bmrgich@mail.io',
         subject: 'welcome',
         text: `Welcome to the app, ${name}.` 
     })
@@ -14,7 +14,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendGoodbyeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'mrgich@mail.io',
+        from: 'bmrgich@mail.io',
         subject: 'Goodbye',
         text: `Sorry to see you go ${name}.`
     })
